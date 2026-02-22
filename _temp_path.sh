db24a8885db61ad2840778191deda9c4964f338f
Yougain <from-github@you.dix.asia>
2026-02-22 16:33:23 +0900

    0.0.7  yougain@Fujitsu

--- _temp_path.sh
+++ _temp_path.sh
@@ -1,8 +1,7 @@
-require fpid
 
+require fpid
 export TMPDIR_YK=/tmp/yk_tmp_dir.${SYS_BOOT_ID}-${FPID}
 mkdir -p "$TMPDIR_YK"
-
 
 temp_path(){
     echo -n "$TMPDIR_YK/$1.$RANDOM.$RANDOM"
--- yk_support.sh
+++ yk_support.sh
@@ -1,9 +1,7 @@
-
+　
 if ! type __yk_support_for_sh_loaded >/dev/null 2>&1; then
-    . /usr/local/lib/yk_support_for_sh/err.sh
+    . "$YK_PREFIX/lib/yk_support_for_sh/err.sh"
     __yk_support_for_sh_loaded(){
-        YK_SUPPORT_LIB_SH=/usr/local/lib/yk_support_for_sh
-        YK_SUPPORT_SHRC_D=/usr/local/etc/yk_support_for_sh/shrc.d
         require(){
             local i
             for i in "$@"; do
@@ -13,40 +11,76 @@ if ! type __yk_support_for_sh_loaded >/dev/null 2>&1; then
                     continue
                 fi
                 local module_name="$i"
-                local yk_module_path="$YK_SUPPORT_LIB_SH/$module_name.sh"
-                local module_content=""
-                if [ -f "$yk_module_path" ]; then
-                    module_content="$(<$yk_module_path)" 2>/dev/null || module_content=""
-                    eval "YK_SUPPORT_FOR_SH_MODULES_LOADED_OF_$i=1"
-                    eval "
-                        __yk_setup_for_require_$i(){
-                            $module_content
-                        }
-                        __yk_setup_for_require_$i
-                        unset -f __yk_setup_for_require_$i
-                    "
+                local XSH=
+                if [ -n "$ZSH_VERSION" ]; then
+                    XSH=zsh
+                elif [ -n "$BASH_VERSION" ]; then
+                    XSH=bash
                 else
-                    err "Module '$module_name' not found at '$yk_module_path'" >&2
+                    XSH=
+                fi
+                local YK_SUPPORT_LIB_SH=
+                local YK_SUPPORT_SHRC_D=
+                local SH=
+                for SH in $XSH sh; do
+                    YK_SUPPORT_LIB_SH="$YK_PREFIX/lib/yk_support_for_$SH"
+                    YK_SUPPORT_SHRC_D="$YK_PREFIX/etc/yk_support_for_$SH/$SH"rc.d
+                    if [ -f "$YK_SUPPORT_LIB_SH/$module_name.$SH" ]; then
+                        local yk_module_path="$YK_SUPPORT_LIB_SH/$module_name.$SH"
+                        local module_content=""
+                        module_content="$(<$yk_module_path)" 2>/dev/null || module_content=""
+                        eval "YK_SUPPORT_FOR_SH_MODULES_LOADED_OF_$i=1"
+                        eval "
+                            __yk_setup_for_require_$i(){
+                                $module_content
+                            }
+                            __yk_setup_for_require_$i
+                            unset -f __yk_setup_for_require_$i
+                        "
+                        break
+                    fi
+                done
+                eval "vnc=\$${vn}"
+                if [ -z "$vnc" ]; then
+                    err "Module '$module_name' not found." >&2
                 fi
             done
         }
         __yk_load_profile_d(){
-            for i in $YK_SUPPORT_SHRC_D/*.sh; do
-                if [ "$i" = "$YK_SUPPORT_SHRC_D/*.sh" ]; then
-                    break
+            local XSH
+            if [ -n "$ZSH_VERSION" ]; then
+                XSH=zsh
+            elif [ -n "$BASH_VERSION" ]; then
+                XSH=bash
+            else
+                XSH=
+            fi
+            local YK_SUPPORT_SHRC_D=
+            local YK_SUPPORT_LIB_SH=
+            local SH=
+            for SH in $XSH sh; do
+                YK_SUPPORT_SHRC_D="$YK_PREFIX/etc/yk_support_for_$SH/$SH"rc.d
+                YK_SUPPORT_LIB_SH="$YK_PREFIX/lib/yk_support_for_$SH"
+                if ! ls "$YK_SUPPORT_SHRC_D"/*."$SH" 1>/dev/null 2>&1; then
+                    continue
                 fi
-                if [ "$(readlink -f "$i" >/dev/null 2>&1)" = "$(readlink -f "$YK_SUPPORT_LIB_SH/$(basename -- "$i")" >/dev/null 2>&1)" ]; then
-                    require $(basename -- "${i%.sh}")
-                else
-                    [ -r "$i" ] && . "$i"
-                fi
+                for i in $YK_SUPPORT_SHRC_D/*.$SH; do
+                    if [ "$i" = "$YK_SUPPORT_SHRC_D/*.$SH" ]; then
+                        break
+                    fi
+                    if [ "$(readlink -f "$i" >/dev/null 2>&1)" = "$(readlink -f "$YK_SUPPORT_LIB_SH/$(basename -- "$i")" >/dev/null 2>&1)" ]; then
+                        require $(basename -- "${i%.$SH}")
+                    else
+                        [ -r "$i" ] && . "$i"
+                    fi
+                done
             done
         }
         if [ -n "$ZSH_VERSION" ]; then
             # zsh
             __yk_load_profile_d
         elif [ -n "$BASH_VERSION" ]; then
-            # bashcd
+            # bash
             __yk_load_profile_d
         fi
     }
