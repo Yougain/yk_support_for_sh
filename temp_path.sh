dbcfad4b7431a8ba8c26ca70a1fc3196619f6e88
Yougain <from-github@you.dix.asia>
2026-03-02 12:57:13 +0900

    0.0.12  yougain@Fujitsu

--- temp_path.sh
+++ temp_path.sh
@@ -2,7 +2,7 @@ require _temp_path end
 
 
 END << 'EOF'
-	#echo `date '+%Y-%m-%d %H:%M:%S.%3N'` zsh[$$] erasing $TMPDIR_YK by END >> "/tmp/zsh_erase_tmp_dir.log"
+	echo `date '+%Y-%m-%d %H:%M:%S.%3N'` zsh\[$$\] erasing $TMPDIR_YK by END >> "/tmp/zsh_erase_tmp_dir.log"
     rm -rf "$TMPDIR_YK" 2>/dev/null || true
 EOF
 
@@ -18,13 +18,14 @@ local dir
 for dir in /tmp/yk_tmp_dir.*; do
     if [ -w "$dir" ];then
         if [ ${dir%-*} != "/tmp/yk_tmp_dir.$SYS_BOOT_ID" ]; then
+			echo `date '+%Y-%m-%d %H:%M:%S.%3N'` zsh\[$$\] erasing $dir by old SYSBOOT_ID >> "/tmp/zsh_erase_tmp_dir.log"
             rm -rf "$dir"
         else
             local fpid="${dir##*-}"
             local pid0="${fpid#*.}"
             local pid1="${pid0%.*}"
             if ! kill -0 "$pid1" >/dev/null 2>&1; then
-				#echo `date '+%Y-%m-%d %H:%M:%S.%3N'` zsh[$$] erasing $dir by old SYS_BOOT_ID >> "/tmp/zsh_erase_tmp_dir.log"
+				echo `date '+%Y-%m-%d %H:%M:%S.%3N'` zsh\[$$\] erasing $dir by non active zsh\[$pid1\] >> "/tmp/zsh_erase_tmp_dir.log"
                 rm -rf "$dir"
             fi
         fi
